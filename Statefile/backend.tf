7e8af397f923334cc36fdf1aac0ad8b7724cadae
SanvithJS <sanvitjs68@gmail.com>
2026-03-22 16:26:11 +0530

    edits

--- Statefile/backend.tf
+++ Statefile/backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     bucket = "sanvith-terraform-statefile-storage"  # Replace with your S3 bucket name
     key    = "statefile/terraform.tfstate"  # Replace with the desired path for your state file
-    region = "us-east-1"  # Replace with your AWS region
-    
+    region = "us-east-1" 
+    dynamodb_table = "terraform_lock"  
+    encrypt = true  # Replace with your DynamoDB table name
   }
 }
--- Statefile/main.tf
+++ Statefile/main.tf
@@ -12,3 +12,13 @@ resource "aws_instance" "EC2" {
 }
 
 
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
